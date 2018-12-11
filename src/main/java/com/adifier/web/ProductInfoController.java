993896dcca033d8c53b182f388d5cbdaf50cab0b
newyorkwo <stephexx@gmail.com>
2018-12-11 18:39:02 +0800

    add getOne

--- src/main/java/com/adifier/web/ProductInfoController.java
+++ src/main/java/com/adifier/web/ProductInfoController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -55,6 +57,27 @@ public class ProductInfoController {
      */
     @GetMapping("/productInfo/input")
     public String inputPage(){
+        return "input";
+    }
+
+
+    /**
+     * post a productInfo datum
+     * @param productInfo
+     * @return
+     */
+    @PostMapping("/productInfo")
+    public String post(ProductInfo productInfo){
+        productInfo.setIndate(new Date());
+        productInfo.setModifiedTime(new Date());
+        productInfoService.save(productInfo);
+        return "redirect:/productInfos";
+    }
+
+    @GetMapping("/productInfo/{productId}/input")
+    public String inputEditPage(@PathVariable long productId, Model model){
+        ProductInfo productInfo=productInfoService.getOne(productId);
+        model.addAttribute("productInfo", productInfo);
         return "input";
     }
 }
