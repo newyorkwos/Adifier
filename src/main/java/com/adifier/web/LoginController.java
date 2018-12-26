2a41eaac5f816f7bce14eb95ae96769a42d34a64
newyorkwo <stephexx@gmail.com>
2018-12-26 16:40:54 +0800

    add getOne

--- src/main/java/com/adifier/web/LoginController.java
+++ src/main/java/com/adifier/web/LoginController.java
@@ -2,6 +2,7 @@ package com.adifier.web;
 
 import com.adifier.domain.User;
 import com.adifier.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +17,11 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class LoginController {
-
+    @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/register")
-    public String register(){
+    public String registerPage(){
         return "register";
     }
 
@@ -28,8 +29,9 @@ public class LoginController {
     public String longPage(){
         return "login";
     }
+
     @PostMapping("/register")
-    public String registerPost(@RequestParam String username,
+    public String register(@RequestParam String username,
                                @RequestParam String password,
                                @RequestParam String email,
                                @RequestParam int phone){
