be8609fb893596adab3987317a73e7d6eff329a5
newyorkwo <stephexx@gmail.com>
2019-01-04 16:26:05 +0800

    add getOne

---
+++ src/main/java/com/adifier/handler/ControllerExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.adifier.handler;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @description: create goble Exception class
+ * @author: StevenWu
+ * @create: 2019-01-04 15:09
+ **/
+
+public class ControllerExceptionHandler {
+
+    public ModelAndView handleException(HttpServletRequest){
+
+    }
+}
